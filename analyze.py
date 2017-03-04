8b232213511078bfa0d09988bb072f8f7c545445
Czxck001 <Czxck001@outlook.com>
2017-03-04 11:10:17 +0800

    refactor statt and analyze

--- analyze.py
+++ analyze.py
@@ -2,50 +2,62 @@
 Analyze the relation of different wordlists and list the words in a
 hierarchical order.
 '''
-import json
-import argparse
-from collections import defaultdict
 
-parser = argparse.ArgumentParser(__doc__)
-parser.add_argument('-l', '--wordlists', nargs='+',
-                    help='list of wordlist json files')
-FLAGS = parser.parse_args()
 
-wordlists = []
-wordcounts = defaultdict(lambda: 0)
-maxcount = 0
-allwords = set()
+def analyze(wordlist_paths, printer=None):
+    import json
+    from collections import defaultdict
 
-# load the wordlists and count the frequency of each word
-for wordlist_path in FLAGS.wordlists:
-    with open(wordlist_path) as f:
-        wl_json = json.load(f)
+    if printer is None:
+        def printer(wordgroup):
+            print(' '.join(sorted(wordgroup)))
 
-    if isinstance(wl_json, dict):
-        wordlist = wl_json.keys()
-    elif isinstance(wl_json, list):
-        wordlist = wl_json
+    wordlists = []
+    wordcounts = defaultdict(lambda: 0)
+    maxcount = 0
+    allwords = set()
 
-    newwords = []
-    for word in wordlist:
-        wordcounts[word] += 1
-        if maxcount < wordcounts[word]:
-            maxcount = wordcounts[word]
+    # load the wordlists and count the frequency of each word
+    for wordlist_path in wordlist_paths:
+        with open(wordlist_path) as f:
+            wl_json = json.load(f)
 
-        # only obtain words not included in previous wordlists
-        if word not in allwords:
-            allwords.add(word)
-            newwords.append(word)
+        if isinstance(wl_json, dict):
+            wordlist = wl_json.keys()
+        elif isinstance(wl_json, list):
+            wordlist = wl_json
 
-    wordlists.append(newwords)
+        newwords = []
+        for word in wordlist:
+            wordcounts[word] += 1
+            if maxcount < wordcounts[word]:
+                maxcount = wordcounts[word]
 
-# group, sort and print
-for k, wordlist in enumerate(wordlists):
-    print('Wordlist {}'.format(k))
-    countwords = defaultdict(lambda: [])
-    for word in wordlist:
-        countwords[wordcounts[word]].append(word)
-    wordgroups = sorted(countwords.items(), key=lambda x: x[0], reverse=True)
-    for lv, wordgroup_kv in enumerate(wordgroups):
-        print('Level {}'.format(lv))
-        print(' '.join(sorted(wordgroup_kv[1])))
+            # only obtain words not included in previous wordlists
+            if word not in allwords:
+                allwords.add(word)
+                newwords.append(word)
+
+        wordlists.append(newwords)
+
+    # group, sort and print
+    for k, wordlist in enumerate(wordlists):
+        print('Wordlist {}'.format(k))
+        countwords = defaultdict(lambda: [])
+        for word in wordlist:
+            countwords[wordcounts[word]].append(word)
+        wordgroups = sorted(countwords.items(),
+                            key=lambda x: x[0],
+                            reverse=True)
+        for lv, wordgroup_kv in enumerate(wordgroups):
+            print('Level {}'.format(lv))
+            printer(wordgroup_kv[1])
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument('-l', '--wordlists', nargs='+',
+                        help='list of wordlist json files')
+    FLAGS = parser.parse_args()
+    analyze(FLAGS.wordlists)
--- statt.py
+++ statt.py
@@ -3,25 +3,6 @@
 ''' Statistic on words of specified wordlist
 '''
 
-import json
-import argparse
-from collections import OrderedDict
-
-
-parser = argparse.ArgumentParser(__doc__)
-parser.add_argument('-l', '--list-path',
-                    help='Path to wordlist JSON file')
-
-FLAGS = parser.parse_args()
-
-# TODO: support multiple wordlists
-obj = json.load(open(FLAGS.list_path))
-
-if isinstance(obj, dict):
-    words = list(obj.keys())
-elif isinstance(obj, list):
-    words = obj
-
 
 def word_length_level(word):
     ''' group the words in terms with their lengths
@@ -35,17 +16,36 @@ def word_length_level(word):
         return 'long'  # long words: length > 8
 
 
-grouped_words = OrderedDict()
-grouped_words['short'] = []
-grouped_words['middle'] = []
-grouped_words['long'] = []
+def group_print(words):
+    from collections import OrderedDict
+    grouped_words = OrderedDict()
+    grouped_words['short'] = []
+    grouped_words['middle'] = []
+    grouped_words['long'] = []
+
+    for word in words:
+        grouped_words[word_length_level(word)].append(word)
+
+    for level, sub_words in grouped_words.items():
+        print('{} words, {} words in total ({:.2f}%)'.format(
+            level, len(sub_words), 100 * len(sub_words) / len(words)
+        ))
+        print(' '.join(sorted(sub_words)))
 
 
-for word in words:
-    grouped_words[word_length_level(word)].append(word)
+if __name__ == '__main__':
+    import json
+    import argparse
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument('-l', '--list-path',
+                        help='Path to wordlist JSON file')
 
-for level, sub_words in grouped_words.items():
-    print('{} words, {} words in total ({:.2f}%)'.format(
-        level, len(sub_words), 100 * len(sub_words) / len(words)
-    ))
-    print(' '.join(sorted(sub_words)))
+    FLAGS = parser.parse_args()
+
+    # TODO: support multiple wordlists
+    obj = json.load(open(FLAGS.list_path))
+
+    if isinstance(obj, dict):
+        words = list(obj.keys())
+    elif isinstance(obj, list):
+        words = obj
