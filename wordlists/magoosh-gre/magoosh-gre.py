5b701fc4f32a74eacab49d30ad8fe30afca0aaf8
Czxck001 <Czxck001@outlook.com>
2017-04-23 17:38:59 +0800

    fix magoosh-gre wordlist

--- wordlists/magoosh-gre/magoosh-gre.py
+++ wordlists/magoosh-gre/magoosh-gre.py
@@ -8,6 +8,7 @@ import json
 from collections import OrderedDict
 parser = argparse.ArgumentParser(__doc__)
 parser.add_argument('-i', '--input',
+                    default='magoosh-gre-1000-words_oct01.txt',
                     help='Input raw text file path')
 parser.add_argument('-o', '--output',
                     help='Output JSON file path')
@@ -15,8 +16,10 @@ parser.add_argument('-o', '--output',
 FLAGS = parser.parse_args()
 
 MAGOOSH_NET = 'gre.magoosh.com'
-MOST_IMPORTANT = 'This word has other definitions but this is the most '
-'important one for the GRE'
+MOST_IMPORTANT = ''.join([
+    'This word has other definitions but this ',
+    'is the most important one for the GRE'
+])
 
 
 word_levels = {'Common (High-frequency) Words',
@@ -45,10 +48,10 @@ with open(FLAGS.input) as f:
             word, part, define = word_define_result[0]
             obj[word] = {
                 'part': part,
-                'definition': define,
                 'level': word_level
             }
             examples = []
+            definitions = [define]
 
             # sometimes the definition of the word exceeds one line. In such
             # case we need to append the following definition lines.
@@ -60,15 +63,19 @@ with open(FLAGS.input) as f:
 
             if line[0].isupper():
                 # this marks the beginning of the example sentence
+                # dump the definition
                 define_finished = True
+                obj[word]['definition'] = ''.join(definitions)
 
             if not define_finished:
-                if obj[word]['definition'].endswith('-'):
+                if definitions[-1].endswith('-'):
                     # if the line is broken at '-', do not append with the
                     # additional space (see word 'spartan')
                     sep = ''
                 else:
                     sep = ' '
+                definitions.append(sep + line)
+
             else:
                 examples.append(line)
                 if line.endswith('.'):
